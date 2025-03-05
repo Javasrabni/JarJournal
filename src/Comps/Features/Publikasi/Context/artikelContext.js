2ae7ec88daba99c3935b369b6abcb2697050e190
Javasrabni <Javasrabnii@gmail.com>
2025-03-05 19:03:51 +0700

    no cache

--- src/Comps/Features/Publikasi/Context/artikelContext.js
+++ src/Comps/Features/Publikasi/Context/artikelContext.js
@@ -30,7 +30,7 @@ export default function ArtikelProvider({ children }) {
             setLoading(true)
             try {
                 const response = await fetch(`${API_URL_PUB}/get/all_user_publikasi`, {
-                    // cache: "no-cache",
+                    cache: "no-cache",
                     method: "GET"
                 })
                 const data = await response.json()
@@ -57,7 +57,7 @@ export default function ArtikelProvider({ children }) {
 
             try {
                 const response = await fetch(`${API_URL_PUB}/get/komentar_publikasi`, {
-                    // cache: "no-cache",
+                    cache: "no-cache",
                     method: "GET"
                 })
                 const data = await response.json()
